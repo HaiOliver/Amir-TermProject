482874f87a49538ea64b2f9cc75f56dad7a9d755
HaiOliver <hhuynh28@my.bcit.ca>
2020-11-26 18:21:07 -0800

    woring on

--- backEnd/server.js
+++ backEnd/server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome page." });
+  res.send({ message: "Welcome page." });
 });
 
 // insert new users
